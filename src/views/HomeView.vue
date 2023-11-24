a0814d06d50a24798584433d3499f5032d1bb925
taniafate <micheartist@yandex.ru>
2023-11-25 00:18:01 +0300

    fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,6 @@
       </div>
     </AppContainer>
   </div>
-  
 </template>
 
 
@@ -17,7 +16,7 @@
 import AppContainer from '../components/AppContainer/AppContainer.vue';
 import PlanetEvents from '../components/PlanetEvents/PlanetEvents.vue';
 import EventSticker from '../components/pictures/EventSticker.vue';
-import { useThemeStore } from '../stores/ThemeSwitcherStore';
+import {useThemeStore} from '../stores/ThemeSwitcherStore';
 
 const themeStore = useThemeStore();
 
