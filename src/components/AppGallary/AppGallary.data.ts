a1cf1e3340d2857470a333231804e31dda6cf869
taniafate <micheartist@yandex.ru>
2023-12-03 13:50:03 +0300

    fix

--- src/components/AppGallary/AppGallary.data.ts
+++ src/components/AppGallary/AppGallary.data.ts
@@ -4,21 +4,21 @@ export const gallaryButtons = [
   {
     RoutesLink: RoutesLinks.VOLCANOES,
     title: 'Volcanoes',
-    bgImage: '@/assets/pictures/volcanoesImage.png'
+    bgImage: '../../assets/pictures/volcanoesImage.png'
   },
   {
     RoutesLink: RoutesLinks.ICE,
     title: 'Sea and Lake Ice',
-    bgImage: '../assets/pictures/iceImage.png'
+    bgImage: '../../assets/pictures/iceImage.png'
   },
   {
     RoutesLink: RoutesLinks.WILDFIRES,
     title: 'Wildfires',
-    bgImage: '/src/assets/pictures/wildfiresImage.png'
+    bgImage: '../../assets/pictures/wildfiresImage.png'
   },
   {
     RoutesLink: RoutesLinks.FLOODS,
     title: 'Floods',
-    bgImage: '/src/assets/pictures/floodsImage.png'
+    bgImage: '../../assets/pictures/floodsImage.png'
   }
 ]
