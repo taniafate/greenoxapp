cbc8907d0add423a0cc21f40f7aeb3c6e13d16d5
taniafate <micheartist@yandex.ru>
2023-12-03 16:24:49 +0300

    fix

--- src/services/axios/planetEvents.ts
+++ src/services/axios/planetEvents.ts
@@ -2,7 +2,7 @@ import type { NASAApiResponse } from "@/types";
 import axios, { type AxiosResponse } from "axios";
 
 const API_BASE_URL = 'https://eonet.gsfc.nasa.gov/api/v2.1/';
-const NASA_API_KEY = import.meta.env.VITE_API_KEY || 'DEMO_KEY';
+const NASA_API_KEY = 'LceEl98yfpeZ0lHvmxEGSET6mAYkci9gLuSB1WM9';
 
 export const apiInstance = axios.create({
   baseURL: API_BASE_URL,
